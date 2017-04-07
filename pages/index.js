b3545966a2d4958997e38c623e85fccdd7764d0d
Alex Price <alex@alexprice.co>
2017-04-06 20:10:56 -0700

    Fixed some launch errors

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Style from '../components/general/style'
 import sheet from '../components/base.scss'
 import * as firebase from 'firebase'
 import Profile from '../components/profile'
-import SignIn from '../components/signin'
+import SignIn from '../components/general/signin'
 
 if (!firebase.apps.length) {
   console.log('%cCreating a new firebase instance...', 'color: grey; font-style: italic;padding: 2px')
@@ -15,56 +15,59 @@ if (!firebase.apps.length) {
     storageBucket: 'expense-9fef4.appspot.com',
     messagingSenderId: '144236555959'
   }
+
   firebase.initializeApp(config)
 }
 
-let userData = {}
-function checkForUser (props) {
-  new Promise((resolve, reject) => {
-    firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-        let displayName = user.displayName
-        let email = user.email
-        let photoURL = user.photoURL
-        let uid = user.uid
-        userData = { uid, displayName, email, photoURL }
-        resolve(userData)
-      } else {
-        reject(new Error(`Firebase error, user not initialized.`))
-      }
-    })
-  }).then(res => {
-    if (!props.userData.uid) props.setUserData(res)
-  }).catch(res => {
-    console.log('%cUser is not signed in.', 'color: dodgerblue')
-  })
-}
-
-function getWelcomeMessage () {
-  return <div className='home'>
-    <div className='intro'>
-      <h1>/expense</h1>
-      <p>An easier way to file expense reports at Formula Slug.</p>
-      <p className='subtext'>
-        Not a member of Formula Slug?
-        <br /><br />
-        We're a sustainable engineering organization at UCSC, working on
-        electric racecars, solar power, transportation, and more.
-        <br /><br />
-        &#62; <a href='mailto:team@formulaslug.com' title='Email the team'>
-        team@formulaslug.com</a>
-      </p>
-    </div>
-
-    <SignIn firebase={firebase} />
-  </div>
-}
-
 class Home extends Component {
-  static async getInitialProps ({ req }) {
-    return req
-      ? { userAgent: req.headers['user-agent'] }
-      : { userAgent: navigator.userAgent }
+  constructor (props, context) {
+    super(props, context)
+    this.state = {
+      user: {}
+    }
+  }
+
+  checkForUser () {
+    console.log('Checking for user...')
+    new Promise((resolve, reject) => {
+      firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+          resolve({
+            name: user.displayName,
+            email: user.email,
+            profilePicture: user.photoURL,
+            uid: user.uid
+          })
+        } else {
+          reject(new Error(`Firebase error, user not initialized.`))
+        }
+      })
+    }).then(res => {
+      this.setState({ user: res })
+    }).catch(res => {
+      console.log('%cUser is not signed in.', 'color: dodgerblue')
+    })
+  }
+
+  getWelcomeMessage () {
+    return <div className='home'>
+      <div className='intro'>
+        <h1>/expense</h1>
+        <p>An easier way to file expense reports at Gesher Group.</p>
+        <p className='subtext'>
+          Not a member of Gesher Group?
+          <br /><br />
+          We're a student-run consultancy at the University of California, at
+          Santa Cruz. If you'd like to get in touch, email us below, or visit our
+          website at geshergroup.org.
+          <br /><br />
+          &#62; <a href='mailto:team@geshergroup.org' title='Email the team'>
+          team@geshergroup.org</a>
+        </p>
+      </div>
+
+      <SignIn firebase={firebase} />
+    </div>
   }
 
   render () {
@@ -74,11 +77,10 @@ class Home extends Component {
       </Head>
       <Style sheet={sheet} />
       { console.log('%cRendering home...', 'color: grey; font-style: italic;padding: 2px') }
-      { (!this.props.userData.uid)
-        ? getWelcomeMessage()
-        : <Profile displayName={this.props.userData.displayName}
-          uid={this.props.userData.uid} firebase={firebase} /> }
-      { (!this.props.userData.uid) ? checkForUser(this.props) : null }
+      { (!this.state.user.uid)
+        ? this.getWelcomeMessage()
+        : <Profile {...this.state} firebase={firebase} /> }
+      { (!this.state.user.uid) ? this.checkForUser() : null }
     </div>
   }
 }
